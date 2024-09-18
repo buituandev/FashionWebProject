bccb3239d635c8ebcc5a29bcb80a519ed1ec921b
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-09-18 16:34:52 +0700

    commit

---
+++ FashionProject/src/test/java/walkonmoon/FashionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package walkonmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
