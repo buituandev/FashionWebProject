2dbbc63d05f5b26d9a7263f5a5ea32899fe63df3
Bui Tuan <147406431+buituandev@users.noreply.github.com>
2024-09-18 12:35:25 +0700

    First commit

---
+++ src/test/java/walkonmoon/fashion/FashionApplicationTests.java
@@ -0,0 +1,13 @@
+package walkonmoon.fashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
