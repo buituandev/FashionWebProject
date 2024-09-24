29bb8b834eb2b2cb6f48c5206da097c1b6943055
Bui Tuan <147406431+buituandev@users.noreply.github.com>
2024-09-24 10:12:13 +0700

    mapping websites version1 09/24

---
+++ src/main/java/walkonmoon/fashion/ClientController.java
@@ -0,0 +1,138 @@
+package walkonmoon.fashion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/index.html")
+    public String indexHtml() {
+        return "index";
+    }
+
+    @GetMapping("/category")
+    public String category() {
+        return "shop-grid";
+    }
+
+    @GetMapping("/shop-grid.html")
+    public String shopGridHtml(){
+        return "shop-grid";
+    }
+
+    @GetMapping("/product-detail")
+    public String productDetail() {
+        return "single-product";
+    }
+
+    @GetMapping("/single-product.html")
+    public String singleProductHtml(){
+        return "single-product";
+    }
+
+    @GetMapping("/shopping-cart")
+    public String shoppingCart() {
+        return "cart";
+    }
+
+    @GetMapping("/cart.html")
+    public String cartHtml(){
+        return "cart";
+    }
+
+    @GetMapping("/shipping-page")
+    public String shippingPage() {
+        return "checkout";
+    }
+
+    @GetMapping("/checkout.html")
+    public String checkoutHtml(){
+        return "checkout";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login.html")
+    public String loginHtml(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/register.html")
+    public String registerHtml(){
+        return "register";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/contact.html")
+    public String contactHtml(){
+        return "contact";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/about.html")
+    public String aboutHtml(){
+        return "about";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog-left-sidebar";
+    }
+
+    @GetMapping("/blog-left-sidebar.html")
+    public String blogLeftSidebarHtml(){
+        return "blog-left-sidebar";
+    }
+
+    @GetMapping("/blog-detail")
+    public String blogDetail() {
+        return "blog-details";
+    }
+
+    @GetMapping("/blog-details.html")
+    public String blogDetailsHtml(){
+        return "blog-details";
+    }
+
+//    @GetMapping("/logout")
+//    public String logout() {
+//        return "logout";
+//    }
+
+//    @GetMapping("/forgot-password")
+//    public String forgotPassword() {
+//        return "forgot-password";
+//    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "my-account";
+    }
+
+    @GetMapping("/my-account.html")
+    public String myAccountHtml(){
+        return "my-account";
+    }
+}
