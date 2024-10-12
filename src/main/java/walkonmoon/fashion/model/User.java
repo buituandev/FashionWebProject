c82ef428c9e86aab10f514d2f37b5827825533d2
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-10-12 11:50:08 +0700

    setup database 2nd 12/10

--- src/main/java/walkonmoon/fashion/model/User.java
+++ src/main/java/walkonmoon/fashion/model/User.java
@@ -1,6 +1,7 @@
 package walkonmoon.fashion.model;
 
 import jakarta.persistence.*;
+import lombok.Cleanup;
 
 import java.util.Date;
 
@@ -13,7 +14,9 @@ public class User {
     @Column
     private Integer id;
     @Column
-    private String name;
+    private String full_name;
+    @Column
+    private String password;
     @Column
     private String gender;
     @Column
@@ -21,15 +24,13 @@ public class User {
     @Column
     private String address;
     @Column
-    private Boolean isDeleted;
-
-    public Boolean getDeleted() {
-        return isDeleted;
-    }
-
-    public void setDeleted(Boolean deleted) {
-        isDeleted = deleted;
-    }
+    private String phone_number;
+    @Column
+    private String image;
+    @Column
+    private int type;
+    @Column
+    private int is_deleted;
 
     public Integer getId() {
         return id;
@@ -39,12 +40,20 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFull_name() {
+        return full_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getGender() {
@@ -70,5 +79,60 @@ public class User {
     public void setAddress(String address) {
         this.address = address;
     }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getIs_deleted() {
+        return is_deleted;
+    }
+
+    public void setIs_deleted(int is_deleted) {
+        this.is_deleted = is_deleted;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column
+    private String province;
+    @Column
+    private String email;
+
+
 
 }
