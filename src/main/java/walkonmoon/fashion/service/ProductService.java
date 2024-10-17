3ad6ebd6b01383f132a698aa1e27df542c313438
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-10-17 15:38:57 +0700

    Update Client UI 2nd

--- src/main/java/walkonmoon/fashion/service/ProductService.java
+++ src/main/java/walkonmoon/fashion/service/ProductService.java
@@ -2,10 +2,10 @@ package walkonmoon.fashion.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import walkonmoon.fashion.model.Category;
 import walkonmoon.fashion.model.Product;
 import walkonmoon.fashion.repository.ProductRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,5 +30,9 @@ public class ProductService {
     public Product getProductById(int id){
         Optional<Product> product = proRepo.findById(id);
         return product.orElse(null);
+    }
+    @Transactional
+    public List<Product> findByCategoryId(int categoryID){
+        return proRepo.findByCategoryId(categoryID);
     }
 }
