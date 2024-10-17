861a0568d4dbb9e50b7199b6a391bc5b69639287
huongpham5515 <phamthuhuong892003@gmail.com>
2024-10-17 13:34:33 +0700

    Edit Product

--- src/main/java/walkonmoon/fashion/service/ProductService.java
+++ src/main/java/walkonmoon/fashion/service/ProductService.java
@@ -2,10 +2,12 @@ package walkonmoon.fashion.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import walkonmoon.fashion.model.Category;
 import walkonmoon.fashion.model.Product;
 import walkonmoon.fashion.repository.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -22,5 +24,11 @@ public class ProductService {
     @Transactional
     public void saveProduct(Product product){
         proRepo.save(product);
+    }
+
+    @Transactional
+    public Product getProductById(int id){
+        Optional<Product> product = proRepo.findById(id);
+        return product.orElse(null);
     }
 }
