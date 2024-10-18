f0823500b9eba10a76b35b68ba9c40c451d8d9f5
Bui Anh Tuan <147406431+buituandev@users.noreply.github.com>
2024-10-18 14:21:55 +0000

    Update

--- src/main/java/walkonmoon/fashion/FirebaseConfig.java
+++ src/main/java/walkonmoon/fashion/FirebaseConfig.java
@@ -3,45 +3,40 @@ package walkonmoon.fashion;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+    private final ResourceLoader resourceLoader;
+    private final String location = "classpath:chroma-config.json";
 
-    private static final String FIREBASE_CONFIG_PATH = "/app/firebase-config.json";
-
-    @PostConstruct
-    public void initialize() {
-        try {
-            logger.info("Looking for Firebase config at: " + FIREBASE_CONFIG_PATH);
-
-            try (FileInputStream serviceAccount = new FileInputStream(FIREBASE_CONFIG_PATH)) {
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                        .setStorageBucket(getBucketName())
-                        .build();
-
-                if (FirebaseApp.getApps().isEmpty()) {
-                    FirebaseApp.initializeApp(options);
-                    logger.info("Firebase initialized successfully.");
-                } else {
-                    logger.info("FirebaseApp already initialized.");
-                }
-            }
-        } catch (IOException e) {
-            logger.error("Failed to initialize Firebase: ", e);
-            throw new RuntimeException("Failed to initialize Firebase", e);
-        }
+    public FirebaseConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
     }
 
+    @PostConstruct
+    public void initialize() throws IOException {
+        Resource resource = resourceLoader.getResource(location);
+        try (InputStream serviceAccount = resource.getInputStream()) {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setStorageBucket(getBucketName())
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        }
+    }
+    
     public String getBucketName() {
         return "chromashop.appspot.com";
     }
