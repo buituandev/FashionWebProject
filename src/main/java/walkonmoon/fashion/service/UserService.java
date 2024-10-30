895c04a2d1b19558f52a55dd47db473ad5a1b81d
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-10-30 15:35:19 +0700

    login 30/10

--- src/main/java/walkonmoon/fashion/service/UserService.java
+++ src/main/java/walkonmoon/fashion/service/UserService.java
@@ -1,10 +1,14 @@
 package walkonmoon.fashion.service;
 
+import jakarta.transaction.Transactional;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 import walkonmoon.fashion.model.User;
 import walkonmoon.fashion.repository.UserRepository;
 
+import java.security.MessageDigest;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,8 +21,31 @@ public class UserService {
     public List<User> getListUser() {
         return (List<User>) userRepo.findAll();
     }
-
+    @Transactional
     public User getUserByEmail(String email) {
         return userRepo.findByEmail(email);
+    }
+    public static String toSHA1(String password) {
+        String salt = "chromashop";
+        String result = null;
+        password = password + salt;
+        try {
+            byte[] dataBytes = password.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            result = Base64.encodeBase64String(md.digest(dataBytes));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    @Transactional
+    public void saveUser(User user) {
+        userRepo.save(user);
+    }
+    @Transactional
+    public User findUserById(int id) {
+        Optional<User> user =  userRepo.findById(id);
+        return user.orElse(null);
     }
 }
