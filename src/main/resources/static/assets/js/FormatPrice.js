df0af446fd3dd410af1cb66acd535bd79e12c078
Bui Anh Tuan <147406431+buituandev@users.noreply.github.com>
2024-11-04 11:28:21 +0700

    Fix cart 1

--- src/main/resources/static/assets/js/FormatPrice.js
+++ src/main/resources/static/assets/js/FormatPrice.js
@@ -1,7 +1,5 @@
 function formatPrices() {
-    console.log("formatPrices function called");
     const priceElements = document.querySelectorAll('.formatted-price');
-    console.log("Number of price elements found:", priceElements.length);
 
     priceElements.forEach(priceElement => {
         const priceText = priceElement.textContent;
@@ -9,17 +7,14 @@ function formatPrices() {
 
         // Check if the price is already formatted
         if (/\d{1,3}(?:\.\d{3})*(?:,\d+)?\s₫/.test(priceText)) {
-            console.log("Price is already formatted:", priceText);
             return;
         }
 
         // Remove non-breaking space and currency symbol
         const cleanedPriceText = priceText.replace(/[\u00A0₫]/g, '').replace(/[^\d.]/g, '');
         const priceValue = parseFloat(cleanedPriceText);
-        console.log("Parsed price value:", priceValue);
 
         const formattedPrice = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(priceValue);
-        console.log("Formatted price:", formattedPrice);
 
         priceElement.textContent = formattedPrice;
     });
